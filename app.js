1c49c23a7d1ac7be4bf0aad4d16362cfb5682bbf
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-11-06 23:31:39 +0530

    socket io added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);  //socket server which integrates with (mounts on) http server
 var hbs = require('express-handlebars');
 var mongoose = require('mongoose');
 
@@ -18,19 +20,12 @@ app.set('view engine', 'hbs');
 
 
 var signupSchema = mongoose.Schema;
-var loginSchema = mongoose.Schema;
 var statsSchema = mongoose.Schema;
 
 var signupSchemaObj = new signupSchema({
 	username: { type: String, required: true, unique: true },
   	userpassword: { type: String, required: true },
     usermail    		: String
-});
-
-var loginSchemaObj = new loginSchema({
-	username: { type: String, required: true, unique: true },
-  	userpassword: { type: String, required: true }
-
 });
 
 var statsSchemaObj = new statsSchema({
@@ -45,7 +40,6 @@ var statsSchemaObj = new statsSchema({
 //creating models
 var Signup = mongoose.model('User', signupSchemaObj);
 var Stats = mongoose.model('userStats', statsSchemaObj);
-var Login = mongoose.model('Login', loginSchemaObj);
 
 //establishing connection
 mongoose.connect(process.env.MONGODB_URI, function (error) {
@@ -58,11 +52,99 @@ mongoose.connect(process.env.MONGODB_URI, function (error) {
 });
 
 
+
+/*Socket.io PART */
+var game_room = {};
+var users = {}; //users list
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  //when the client emits play, this listenes and executes
+  socket.on('create',function(uniquekey){
+
+    socket.uniquekey = uniquekey; //a unique key corresponding to this game
+
+    game_room[uniquekey] = uniquekey;
+    socket.room = game_room[uniquekey];
+    console.log(socket.room);
+
+    //send client to room game_room[uniquekey]
+    socket.join(game_room[uniquekey]);
+
+    //echo to client they have connected!
+    socket.emit('updatechat','SERVER','You have connected to room' + game_room[uniquekey]);
+
+    //echo to room1 that a person has connected to room1
+    socket.broadcast.to(game_room[uniquekey]).emit('updatechat','SERVER',"user_name" + 'has created this room');
+
+    //socket.emit('updaterooms',rooms,'room1');
+
+  });
+
+
+
+  //when the client emits join, this listenes and executes
+  socket.on('join',function(uniquekey){
+
+    //stores the username in socket session for this client
+    console.log(uniquekey);
+  //  socket.username = user_name;
+    //send client to room1
+    socket.room = game_room[uniquekey];
+    socket.join(game_room[uniquekey]);
+    console.log(socket.room);
+
+    //echo to client they have connected!
+    socket.emit('updatechat','SERVER','You have connected to '+ game_room[uniquekey]);
+
+    //echo to room1 that a person has connected to room1
+
+    socket.broadcast.to(game_room[uniquekey]).emit('updatechat','SERVER',"user_name" + 'has joined this room');
+
+  //  socket.emit('updaterooms',rooms,'room1');
+
+  });
+
+
+  //when the client emits send move, this listenes and executes
+  socket.on('sendmove',function(source, target, uniquekey){
+
+    //we tell the client to execute 'updateboard' with the parameters
+    console.log(source + " " + target);
+   // console.log(game_room[uniquekey]);
+    io.sockets.in(game_room[uniquekey]).emit('updateboard', source, target);
+
+  });
+
+
+  socket.on('whosechance',function(currplayer, uniquekey){
+    console.log('in whose chance');
+    io.sockets.in(game_room[uniquekey]).emit('flipchance',currplayer);
+  })
+
+  socket.on('sendchat',function(msg, uniquekey){
+    io.sockets.in(game_room[uniquekey]).emit('updatechatui',msg);
+  });
+
+  //when the user disonnects... perform this
+  socket.on('disonnect',function(){
+    //echo globally that this client has left
+    socket.broadcast.emit('updatechat','SERVER', " disonnected");
+    socket.leave(socket.room);
+  });
+
+
+});
+
+
+
+
+/* Routing PART*/
+
 app.get('/home',function(req,res){
-  // console.log(req.params);
   res.render('home'/*, {id: req.params.id, user: req.params.user
   }*/);
-  //res.status(200).send(html);
 })
 
 app.post('/handleSignup',function(req,res){
@@ -71,16 +153,11 @@ app.post('/handleSignup',function(req,res){
   result.save(function(err){
   	if(err)
   	{
-  		//signup unsuccessfull
-  		//redirect to same page
   		console.log(err);
   		res.redirect("/authenticateUser.html");
   	}
   	else
   	{
-  		//signup successfull
-  		//redirect to home page
-  		// console.log()
   		var gameStats = new Stats({
   			username : req.body.username,
   			totalGames : 0,
@@ -100,13 +177,9 @@ app.post('/handleSignup',function(req,res){
 app.post('/handleSignin',function(req,res){
 	Signup.find(req.body, function(err, userDetails){
 		if(err){
-			//login unsuccessfull
-			//redirect to same page
 			res.redirect("/authenticateUser.html");
 		}	
 		else{
-			//login successful
-			//redirect to home page
 			console.log(userDetails);
 			var x = Stats.find({username:req.body.username}, function(err, userGameDetails)
 			{
@@ -114,6 +187,13 @@ app.post('/handleSignin',function(req,res){
 			});
 		}
 	});
+})
+
+app.get('/:id/:playas',function(req,res){
+  console.log(req.params);
+  res.render('play', {id: req.params.id,
+    playas: req.params.playas
+  });
   //res.status(200).send(html);
 })
 
